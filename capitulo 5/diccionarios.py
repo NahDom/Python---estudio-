e4501afb3ee3bffac86a5b7a27a538fa41aab12c
GalaticPizza <Clastik-mor13@outlook.com.ar>
2025-04-10 00:31:33 -0300

    cambios varios xD

---
+++ capitulo 5/diccionarios.py
@@ -0,0 +1,6 @@
+# los diccionarios, una estructura que almacena sus elementos, en estructuras de tipo clave:valor,  El valor de una clave puede ser un número, una cadena, una lista o incluso otro diccionario.
+# diccionario = {clave:valor}
+
+alien = {'color': 'verde'}
+# acceder al valor del diccionario, se accede por medio de la clave entre corchetes --> diccionario[clave]
+print(alien['color'])
