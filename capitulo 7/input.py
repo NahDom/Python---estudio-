089078a26504a747d6bf1b58f8e7543a2e587413
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2025-09-11 22:33:00 -0300

    xD

---
+++ capitulo 7/input.py
@@ -0,0 +1,16 @@
+"""
+    Entrada del usuario 
+    En este capitulo se ahonda sobre el bucle while
+    la funcion input y la interactividad de los programas en python
+"""
+# primer ejemplo
+
+mensaje  = input("Dime algo y lo repetire: ")
+print(mensaje)
+
+# interesante no sabia que python permitia una concatenacion de texto de tipo lineas subsecuentes
+
+edad = int(input('ingrese su edad: '))
+print(type(edad))
+print(edad)
+print(edad >= 18)
---
+++ capitulo 7/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+edad = int(input('ingrese su edad: '))
+print(type(edad))
+print(edad)
+print(edad >= 18)
