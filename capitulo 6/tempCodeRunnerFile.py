53d13ea7ffaa0c028b25e51ee8620f11dcde2f4f
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2025-09-11 21:39:13 -0300

    xD

--- capitulo 6/tempCodeRunnerFile.py
+++ capitulo 6/tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
+    
 favorite_languages = {
     'jen': ['python', 'rust'],
     'sarah': ['c'],
@@ -6,8 +7,13 @@ favorite_languages = {
 }
 
 for name, languages in favorite_languages.items():
+    if len(languages) >= 1:
     # la unica forma de acceder a los valores completos ya que esto es una lista de elementos en cada clave de nombres, lo que ocurre aqui
     # es que el segundo elemento que va a tener el valor de los valores, es quien va a ser recorrido en un for interno por ser una lista
-    print(f"\n {name.title()} tus lenguajes favoritos son: ")
-    for language in languages:
-        print(f"\t {language.title()}")
+        print(f"\n {name.title()} tus lenguajes favoritos son: ")
+        for language in languages:
+            print(f"\t {language.title()}")
+    else:
+        print(f"\n {name.title()} tu lenguaje favorito es: ")
+        for language in languages:
+            print(f"\t {language.title()}")
