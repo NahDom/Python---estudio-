a17ba2ae9ea92510ca6bf5b09da0d7636629f2c5
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2025-09-22 20:36:04 -0300

    adhiriendo codigo

--- capitulo 7/input.py
+++ capitulo 7/input.py
@@ -14,3 +14,24 @@ edad = int(input('ingrese su edad: '))
 print(type(edad))
 print(edad)
 print(edad >= 18)
+
+
+print(" Mesa para varios")
+cantidad_personas = int(input("\tPor favor indique cuantos comeran: "))
+if cantidad_personas >= 8:
+    print(" Perdonen deberan esperar a la liberacion de la mesa")
+else:
+    print("Su mesa esta lista")
+    
+val = 1
+val2 = 2
+
+print(f"valor de: {val / val2} y el modulo de {val % val2}")
+
+print(val & val2) # this is a bitwice operator: toma cada pareja de bits de los operandos y solo retorna 1 si ambos bits son también 1
+ # 0001 # 0010
+ 
+numero_actual = 1
+while numero_actual <= 5:
+    print(numero_actual)
+    numero_actual += 1
