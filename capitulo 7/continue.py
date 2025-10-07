59abdac60a19a62ff1e4a8bcee532a7e2d41d230
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2025-10-06 23:42:34 -0300

    add new project

--- capitulo 7/continue.py
+++ capitulo 7/continue.py
@@ -1,10 +1,9 @@
 # imprimier un bucle que va del 1 al 10 pero imprima solo los impares
 current_number = 0
-while current_number < 10:
+while current_number < 20:
     current_number += 1
     if current_number % 2 == 0:
         continue
     # el % es el modulo en la mayoria de lenguajes 
     # mientras que / es la division entera
-    print(current_number) 
-#deberia de imprimirlos por fuera
+    print(current_number) # deberia de imprimirlos aqui, solo lo realiza por dentro
