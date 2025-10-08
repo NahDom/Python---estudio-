61162928972e8b6eea05c8e4d2d41d259015ea59
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2025-10-08 01:11:10 -0300

    adding

---
+++ capitulo 7/no hay pasteles.py
@@ -0,0 +1,8 @@
+postres_no_terminados = ['pastrami','Flan con dulce de leche','pastrami', 'helado artesanal', 'alfajores', 'churros rellenos', 'pastelitos criollos','pastrami','pastrami','pastrami','pastrami']
+
+print("\nYa no nos queda pastrami")
+
+while 'pastrami' in postres_no_terminados:
+    postres_no_terminados.remove('pastrami')
+
+print(postres_no_terminados)
