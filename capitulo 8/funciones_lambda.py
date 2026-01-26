8ee1cfc3fcb6ecc49c483eba018f0df05f2925a8
Galatic Pizza <Clastik-mor13@outlook.com.ar>
2026-01-26 15:28:05 -0300

    Add

--- capitulo 8/funciones_lambda.py
+++ capitulo 8/funciones_lambda.py
@@ -1,5 +1,22 @@
 """
-    En este apartado me dedico al estudio de las funciones lambda en python
+    En este apartado me dedico al estudio de las funciones en python
 """
-# definicion de una funcion lamda cualquiera
+# definicion de una funcion cualquiera
 
+# Saludo
+
+def saludo(username):
+    print(f"hello, {username}")
+    
+saludo("Nahuel")
+
+def libro(libro):
+    print(f"Uno de mis libros favoritos es {libro}")
+    
+libro("Gente Ansiosa")
+
+def describe_mascot(animal, name):
+    print(f"\nI have an {animal}")
+    print(f"My {animal}'s name is {name.title()}")
+    
+describe_mascot("Dog", "Toto")
---
+++ capitulo 8/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def describe_mascot(animal, name):
+    print(f"\nI have an {animal}")
+    print(f"My {animal}'s name is {name.title()}")
+    
+describe_mascot("Dog", "Toto")
